43f0f56a567799af834c17046bb1a31f52486875
janderTodero <jandertodero@gmail.com>
2025-10-20 15:24:18 -0300

    Add public movie listing with pagination

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,11 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.page(params[:page]).per(6)
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+    @comments = @movie.comments.order(created_at: :desc)
+    @comments = Comment.new
+  end
+end
