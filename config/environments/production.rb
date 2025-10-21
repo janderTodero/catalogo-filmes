439257e91291f1ae63b4355b23f4fd8293d87bb5
janderTodero <jandertodero@gmail.com>
2025-10-21 15:38:22 -0300

    Configure Action Mailer for password recovery

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,20 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: ENV['APP_HOST'] || 'your-app.onrender.com' }
+  
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            ENV['SMTP_USERNAME'],
+    password:             ENV['SMTP_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
