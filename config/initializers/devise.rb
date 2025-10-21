52501d60d76cc0b6a53d79609b73a9195c8a02b3
janderTodero <jandertodero@gmail.com>
2025-10-21 18:25:51 -0300

    email fix 5

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'jaquinharua10@gmail.com'
+  config.mailer_sender = 'MS_xYyOMk@test-zkq340e8w23gd796.mlsender.net'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
