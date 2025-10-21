5817c44d175fc4c1d894d07bb3c11ff6875b208d
janderTodero <jandertodero@gmail.com>
2025-10-21 16:20:11 -0300

    Fix password recovery 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,19 +4,19 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: ENV['APP_HOST'] || 'https://catalogo-filmes-tej3.onrender.com/' }
-  
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            ENV['SMTP_USERNAME'],
-    password:             ENV['SMTP_PASSWORD'],
-    authentication:       'plain',
-    enable_starttls_auto: true
-  }
+config.action_mailer.smtp_settings = {
+  user_name: 'apikey',             # literal 'apikey'
+  password: ENV['SENDGRID_API_KEY'], # a API Key que você gerou
+  domain: 'catalogo-filmes-tej3.onrender.com',
+  address: 'smtp.sendgrid.net',
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
+}
+
+config.action_mailer.default_url_options = { host: 'catalogo-filmes-tej3.onrender.com', protocol: 'https' }
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = true
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
