e04546ad07ef0fffe1fd519b7a8075ebbfc632bd
janderTodero <jandertodero@gmail.com>
2025-10-22 22:20:57 -0300

    starting internationalization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -6,5 +7,18 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
     devise_parameter_sanitizer.permit(:account_update, keys: [ :name ])
+  end
+
+  def set_locale
+    # Pega o locale da URL, converte em símbolo e só aplica se estiver nos disponíveis
+    if params[:locale].present? && I18n.available_locales.include?(params[:locale].to_sym)
+      I18n.locale = params[:locale].to_sym
+    else
+      I18n.locale = I18n.default_locale
+    end
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
   end
 end
